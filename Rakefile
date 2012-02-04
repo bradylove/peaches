472287f90c4ae906cd21df693edbc09f96c2cfd3
Brady Love <love.brady@gmail.com>
2012-02-04 00:53:00 -0800

    Added binn files

--- Rakefile
+++ Rakefile
@@ -16,9 +16,10 @@ Jeweler::Tasks.new do |gem|
   gem.homepage = "http://github.com/bradylove/peaches"
   gem.license = "MIT"
   gem.summary = %Q{A simple fuzzy file finder written in Ruby}
-  gem.description = %Q{TODO: write a description}
+  gem.description = %Q{write a description}
   gem.email = "love.brady@gmail.com"
   gem.authors = ["Brady Love"]
+  gem.executables = ['peaches']
   # Include your dependencies below. Runtime dependencies are required when using your gem,
   # and development dependencies are only needed for development (ie running rake tasks, tests, etc)
   #  gem.add_runtime_dependency 'jabber4r', '> 0.1'
--- spec/peaches/finder_spec.rb
+++ spec/peaches/finder_spec.rb
@@ -13,10 +13,10 @@ describe "Peaches::Finder" do
   end
 
   it "should find a 2nd level file or directory with fuzzy name" do
-    finder.find("pches", root).should == ["lib/peaches/finder.rb", "lib/peaches/version.rb", "lib/peaches.rb", "spec/peaches/finder_spec.rb", "spec/peaches_spec.rb"]
+    finder.find("pches", root).should == ["bin/peaches", "lib/peaches/finder.rb", "lib/peaches/version.rb", "lib/peaches.rb", "spec/peaches/finder_spec.rb", "spec/peaches_spec.rb"]
   end
 
   it "should find a file without having the first letter of the file" do
-    finder.find("spec", root).should == [".rspec", "spec/peaches/finder_spec.rb", "spec/peaches_spec.rb", "spec/spec_helper.rb"]
+    finder.find("spec", root).should == ["spec/peaches/finder_spec.rb", "spec/peaches_spec.rb", "spec/spec_helper.rb"]
   end
 end
