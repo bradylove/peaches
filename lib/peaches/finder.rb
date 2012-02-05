00fd9be8629bc6ab67c682c00c2c43b870c80602
Brady Love <love.brady@gmail.com>
2012-02-04 16:19:23 -0800

    Added docs and minor changes

--- lib/peaches/finder.rb
+++ lib/peaches/finder.rb
@@ -1,12 +1,42 @@
 module Peaches
   class Finder
-    def find(string, root)
+    attr_accessor :cache
+
+    def initialize(root)
+      @root = root
+    end
+
+
+    # FuzzyFind files from the cache
+    #
+    # == Paarameters
+    # string::
+    #   The string you want to use to find matching files, if left black it
+    #   will bring up all the files
+    def find(string)
       string = string.split(//).join(".*?")
       pattern = "/#{string}/i"
 
+      results = self.cache.grep /#{string}/i
+
+      return results
+    end
+
+    # forces a refresh of the file list cache
+    def refresh_cache
+      @cache = build_cache
+    end
+
+    # Get and/or fetch the file list cache
+    def cache
+      @cache ||= build_cache
+    end
+
+    # Builds the cache excluding files that start with .
+    def build_cache
       all_files = []
 
-      Find.find(root) do |path|
+      Find.find(@root) do |path|
         if FileTest.directory?(path)
           if File.basename(path)[0] == ?.
             Find.prune
@@ -14,14 +44,12 @@ module Peaches
             next
           end
         else
-          path.slice!("#{root}/")
+          path.slice!("#{@root}/")
           all_files << path
         end
       end
 
-      results = all_files.grep /#{string}/i
-
-      return results
+      return all_files
     end
   end
 end
--- spec/peaches/finder_spec.rb
+++ spec/peaches/finder_spec.rb
@@ -2,21 +2,21 @@ require 'spec_helper'
 
 describe "Peaches::Finder" do
   let(:root) { "/Users/bradylove/Development/Gems/peaches" }
-  let(:finder) { Peaches::Finder.new }
+  let(:finder) { Peaches::Finder.new(root) }
 
   it "should find a toplevel file with a fuzzy name" do
-    finder.find("gem", root).should == ["Gemfile", "Gemfile.lock"]
+    finder.find("gem").should == ["Gemfile", "Gemfile.lock"]
   end
 
   it "should find a top level directory with a fuzzy name" do
-    finder.find("lb", root).should == ["lib/peaches/finder.rb", "lib/peaches/version.rb", "lib/peaches.rb", "spec/spec_helper.rb"]
+    finder.find("lb").should == ["lib/peaches/finder.rb", "lib/peaches/version.rb", "lib/peaches.rb", "spec/spec_helper.rb"]
   end
 
   it "should find a 2nd level file or directory with fuzzy name" do
-    finder.find("pches", root).should == ["bin/peaches", "lib/peaches/finder.rb", "lib/peaches/version.rb", "lib/peaches.rb", "spec/peaches/finder_spec.rb", "spec/peaches_spec.rb"]
+    finder.find("pches").should == ["bin/peaches", "lib/peaches/finder.rb", "lib/peaches/version.rb", "lib/peaches.rb", "spec/peaches/finder_spec.rb", "spec/peaches_spec.rb"]
   end
 
   it "should find a file without having the first letter of the file" do
-    finder.find("spec", root).should == ["spec/peaches/finder_spec.rb", "spec/peaches_spec.rb", "spec/spec_helper.rb"]
+    finder.find("spec").should == ["spec/peaches/finder_spec.rb", "spec/peaches_spec.rb", "spec/spec_helper.rb"]
   end
 end
